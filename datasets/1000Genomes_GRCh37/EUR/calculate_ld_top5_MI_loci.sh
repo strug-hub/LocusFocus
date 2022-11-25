835ab3da3edd460693a5d8faa90422245e64b88a
Mackenzie Ian Frew <mackenzie.frew@sickkids.ca>
2022-11-25 13:16:27 -0500

    Fix dev docker compose - Move user data, datasets out of flask dir; use docker volumes instead

---
+++ datasets/1000Genomes_GRCh37/EUR/calculate_ld_top5_MI_loci.sh
@@ -0,0 +1,84 @@
+plink	--noweb \
+	--bfile chr1 \
+	--ld-snp chr1:205906897 \
+	--r2 \
+	--ld-window-r2 0 \
+	--ld-window 999999 \
+	--ld-window-kb 99999 \
+	--out rs7549173_slc26a9
+
+plink 	--noweb \
+	--bfile chrX \
+	--ld-snp chr23:115566839 \
+	--r2 \
+	--ld-window-r2 0 \
+	--ld-window 999999 \
+	--ld-window-kb 99999 \
+	--out rs3788766_slc6a14
+
+plink 	--noweb \
+	--bfile chr7 \
+	--ld-snp chr7:142498523 \
+	--r2 \
+	--ld-window-r2 0 \
+	--ld-window 999999 \
+	--ld-window-kb 99999 \
+	--out rs1799886_prss1
+
+plink 	--noweb \
+	--bfile chr13 \
+	--ld-snp chr13:25282819 \
+	--r2 \
+	--ld-window-r2 0 \
+	--ld-window 999999 \
+	--ld-window-kb 99999 \
+	--out rs61948108_atp12a
+
+plink   --noweb \
+        --bfile chr20 \
+        --ld-snp chr20:48833957 \
+        --r2 \
+        --ld-window-r2 0 \
+        --ld-window 999999 \
+        --ld-window-kb 99999 \
+        --out rs867489_cebpb
+
+plink   --noweb \
+        --bfile chrX \
+        --ld-snp chr23:115362010 \
+        --r2 \
+        --ld-window-r2 0 \
+        --ld-window 999999 \
+        --ld-window-kb 99999 \
+        --out rs5952115_agtr2
+
+
+plink   --noweb \
+        --bfile chrX \
+        --ld-snp chr23:115365487 \
+        --r2 \
+        --ld-window-r2 0 \
+        --ld-window 999999 \
+        --ld-window-kb 99999 \
+        --out rs6520193_agtr2
+
+
+plink   --noweb \
+        --bfile chrX \
+        --ld-snp chr23:115343877 \
+        --r2 \
+        --ld-window-r2 0 \
+        --ld-window 999999 \
+        --ld-window-kb 99999 \
+        --out rs7061667_agtr2
+
+
+plink   --noweb \
+        --bfile chrX \
+        --ld-snp chr23:115386565 \
+        --r2 \
+        --ld-window-r2 0 \
+        --ld-window 999999 \
+        --ld-window-kb 99999 \
+        --out rs5952223_lung_Paul_top_SNP_r2
+
