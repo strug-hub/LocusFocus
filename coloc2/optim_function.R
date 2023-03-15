8d06a7ef6fe3011523c95e61c3081e3cceb84f92
Mackenzie Ian Frew <mackenzie.frew@sickkids.ca>
2023-03-15 15:59:09 -0400

    Format R scripts - Ignore "=" into "<-" to avoid any functional changes from formatting

--- coloc2/optim_function.R
+++ coloc2/optim_function.R
@@ -7,21 +7,21 @@ fn = function(p, data) {
   a2 = p[3]
   a3 = p[4]
 
-   #p0 <- 1 - (p1 + p2 + p12 + p1*p2)
-  suma = sum(exp(c(a0,a1,a2,a3,a1+a2)))
+  # p0 <- 1 - (p1 + p2 + p12 + p1*p2)
+  suma = sum(exp(c(a0, a1, a2, a3, a1 + a2)))
 
-   lkl.frame.temp <- as.matrix(data)
-   lkl.frame.temp[,1] <- lkl.frame.temp[,1] + log(exp(a0)/suma)
-   lkl.frame.temp[,2] <- lkl.frame.temp[,2] + log(exp(a1)/suma)
-   lkl.frame.temp[,3] <- lkl.frame.temp[,3] + log(exp(a2)/suma)
-   lkl.frame.temp[,4] <- lkl.frame.temp[,4] + log(exp(a1)/suma) + log(exp(a2)/suma)
-   lkl.frame.temp[,5] <- lkl.frame.temp[,5] + log(exp(a3)/suma)
+  lkl.frame.temp <- as.matrix(data)
+  lkl.frame.temp[, 1] <- lkl.frame.temp[, 1] + log(exp(a0) / suma)
+  lkl.frame.temp[, 2] <- lkl.frame.temp[, 2] + log(exp(a1) / suma)
+  lkl.frame.temp[, 3] <- lkl.frame.temp[, 3] + log(exp(a2) / suma)
+  lkl.frame.temp[, 4] <- lkl.frame.temp[, 4] + log(exp(a1) / suma) + log(exp(a2) / suma)
+  lkl.frame.temp[, 5] <- lkl.frame.temp[, 5] + log(exp(a3) / suma)
 
-   sumlkl = sum(apply(lkl.frame.temp, MAR = 1, FUN = logsum))
-   # penalizing by adding a huge number
-   #sumlkl = sum(apply(lkl.frame.temp, MAR = 1, FUN = logsum)) - (10^4)*abs(1-p0-p1-p2-p3-p4)
+  sumlkl = sum(apply(lkl.frame.temp, MAR = 1, FUN = logsum))
+  # penalizing by adding a huge number
+  # sumlkl = sum(apply(lkl.frame.temp, MAR = 1, FUN = logsum)) - (10^4)*abs(1-p0-p1-p2-p3-p4)
 
-   return(sumlkl)
+  return(sumlkl)
 }
 
 fn.pw.gwas = function(p, data) {
@@ -30,21 +30,21 @@ fn.pw.gwas = function(p, data) {
   a2 = p[3]
   a3 = p[4]
   a4 = p[5]
-  #print(nrow(data))
-  suma = sum(exp(c(a0,a1,a2,a3,a4)))
-  #print(paste("Alphas:" , exp(a0)/suma,exp(a1)/suma,exp(a2)/suma,exp(a3)/suma,exp(a4)/suma), sep=" ")
+  # print(nrow(data))
+  suma = sum(exp(c(a0, a1, a2, a3, a4)))
+  # print(paste("Alphas:" , exp(a0)/suma,exp(a1)/suma,exp(a2)/suma,exp(a3)/suma,exp(a4)/suma), sep=" ")
   lkl.frame.temp <- as.matrix(data)
-  lkl.frame.temp[,1] <- log(exp(a0)/suma)
-  lkl.frame.temp[,2] <- lkl.frame.temp[,2] + log(exp(a1)/suma)
-  lkl.frame.temp[,3] <- lkl.frame.temp[,3] + log(exp(a2)/suma)
-  lkl.frame.temp[,4] <- lkl.frame.temp[,4] + log(exp(a3)/suma)
-  lkl.frame.temp[,5] <- lkl.frame.temp[,5] + log(exp(a4)/suma)
-  #print(lkl.frame.temp[1,])
-  #print(apply(lkl.frame.temp, MAR = 1, FUN = logsum))
-  #print(log(sum(exp(lkl.frame.temp[1,]))))
+  lkl.frame.temp[, 1] <- log(exp(a0) / suma)
+  lkl.frame.temp[, 2] <- lkl.frame.temp[, 2] + log(exp(a1) / suma)
+  lkl.frame.temp[, 3] <- lkl.frame.temp[, 3] + log(exp(a2) / suma)
+  lkl.frame.temp[, 4] <- lkl.frame.temp[, 4] + log(exp(a3) / suma)
+  lkl.frame.temp[, 5] <- lkl.frame.temp[, 5] + log(exp(a4) / suma)
+  # print(lkl.frame.temp[1,])
+  # print(apply(lkl.frame.temp, MAR = 1, FUN = logsum))
+  # print(log(sum(exp(lkl.frame.temp[1,]))))
   sumlkl = sum(apply(lkl.frame.temp, MAR = 1, FUN = logsum))
-  sumlkl = sumlkl + sum(log(c(dnorm(p[1], mean=2, sd=3), (dnorm(p[2:5], mean=-2, sd=3)))))
-  #print(sumlkl)
+  sumlkl = sumlkl + sum(log(c(dnorm(p[1], mean = 2, sd = 3), (dnorm(p[2:5], mean = -2, sd = 3)))))
+  # print(sumlkl)
   return(sumlkl)
 }
 
@@ -54,21 +54,21 @@ fn.three.hyp = function(p, data) {
   a2 = p[3]
   a3 = p[4]
 
-   #p0 <- 1 - (p1 + p2 + p12 + p1*p2)
-  suma = sum(exp(c(a0,a1,a2,a3)))
+  # p0 <- 1 - (p1 + p2 + p12 + p1*p2)
+  suma = sum(exp(c(a0, a1, a2, a3)))
 
-   lkl.frame.temp <- as.matrix(data)
-   lkl.frame.temp[,1] <- lkl.frame.temp[,1] + log(exp(a0)/suma)
-   lkl.frame.temp[,2] <- lkl.frame.temp[,2] + log(exp(a1)/suma)
-   lkl.frame.temp[,3] <- lkl.frame.temp[,3] + log(exp(a2)/suma)
-   lkl.frame.temp[,4] <- logsum(c(lkl.frame.temp[,4], lkl.frame.temp[,5])) + log(exp(a3)/suma)
-   lkl.frame.temp[,5] <- 1
+  lkl.frame.temp <- as.matrix(data)
+  lkl.frame.temp[, 1] <- lkl.frame.temp[, 1] + log(exp(a0) / suma)
+  lkl.frame.temp[, 2] <- lkl.frame.temp[, 2] + log(exp(a1) / suma)
+  lkl.frame.temp[, 3] <- lkl.frame.temp[, 3] + log(exp(a2) / suma)
+  lkl.frame.temp[, 4] <- logsum(c(lkl.frame.temp[, 4], lkl.frame.temp[, 5])) + log(exp(a3) / suma)
+  lkl.frame.temp[, 5] <- 1
 
-   sumlkl = sum(apply(lkl.frame.temp, MAR = 1, FUN = logsum))
-   # penalizing by adding a huge number
-   #sumlkl = sum(apply(lkl.frame.temp, MAR = 1, FUN = logsum)) - (10^4)*abs(1-p0-p1-p2-p3-p4)
+  sumlkl = sum(apply(lkl.frame.temp, MAR = 1, FUN = logsum))
+  # penalizing by adding a huge number
+  # sumlkl = sum(apply(lkl.frame.temp, MAR = 1, FUN = logsum)) - (10^4)*abs(1-p0-p1-p2-p3-p4)
 
-   return(sumlkl)
+  return(sumlkl)
 }
 
 
@@ -79,16 +79,14 @@ fn.per.SNP = function(p, data) {
   p2 = p[3]
   p3 = p[4]
 
-  sump = sum(exp(c(p0,p1,p2,p3)))
+  sump = sum(exp(c(p0, p1, p2, p3)))
 
   lkl.frame.temp <- as.matrix(data)
-  lkl.frame.temp[,1] <- lkl.frame.temp[,1] + log(exp(p0)/sump)
-  lkl.frame.temp[,2] <- lkl.frame.temp[,2] + log(exp(p1)/sump)
-  lkl.frame.temp[,3] <- lkl.frame.temp[,3] + log(exp(p2)/sump)
-  lkl.frame.temp[,5] <- lkl.frame.temp[,5] + log(exp(p3)/sump)
+  lkl.frame.temp[, 1] <- lkl.frame.temp[, 1] + log(exp(p0) / sump)
+  lkl.frame.temp[, 2] <- lkl.frame.temp[, 2] + log(exp(p1) / sump)
+  lkl.frame.temp[, 3] <- lkl.frame.temp[, 3] + log(exp(p2) / sump)
+  lkl.frame.temp[, 5] <- lkl.frame.temp[, 5] + log(exp(p3) / sump)
 
   # penalizing by adding a huge number
-  sumlkl = sum(apply(lkl.frame.temp, MAR = 1, FUN = logsum)) 
+  sumlkl = sum(apply(lkl.frame.temp, MAR = 1, FUN = logsum))
 }
-
-
