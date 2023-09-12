ab7937f436b1ee2c4bb577d73e447e75851b35fa
Mackenzie Ian Frew <mackenzie.frew@sickkids.ca>
2023-09-12 10:37:11 -0400

    Add support for HTTP security headers - Move GA script to js file - Add Flask-Talisman to requirements

---
+++ static/js/ga/google_analytics.js
@@ -0,0 +1,11 @@
+/**
+ * Google tag handler for all HTML pages.
+ */
+
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag("js", new Date());
+
+gtag("config", "G-LCN3CN42ZL");
