587ac856db57f4da7853331659100b3b0c6283ae
Mackenzie Ian Frew <mackenzie.frew@sickkids.ca>
2023-09-27 15:16:23 -0400

    Make plot.html check for set-based test case

--- static/js/params_table.js
+++ static/js/params_table.js
@@ -1,4 +1,4 @@
-function buildParamsTable(data, sessionid) {
+function buildParamsTable(data, sessionid, type = "default") {
   var tableselect = d3.select("#params-table");
 
   // Column headers
@@ -15,34 +15,44 @@ function buildParamsTable(data, sessionid) {
   // Table body:
   let tbody = tableselect.append("tbody");
   // field-value pairs in order displayed in table
-  let table_data = [
-    ["Session ID", sessionid],
-    ["Lead SNP", data["lead_snp"]],
-    ["Chromosome", chrom],
-    ["Start position", startbp],
-    ["End position", endbp],
-    ["Build", data["coordinate"]],
-    ["Infer variants", data["inferVariant"]],
-    [`Number of SNPs in ${chrom}:${startbp}-${endbp}`, data["snps"].length],
-    ["LD Population", data["ld_populations"]],
-    ["GTEx version", data["gtex_version"]],
-    ["Number of GTEx tissues selected", data["gtex_tissues"].length],
-    [
-      "Number of GTEx genes selected",
-      data["gtex_genes"] !== undefined ? data["gtex_genes"].length : undefined,
-    ],
-    ["SS region", data["SS_region"]],
-    [`Number of SNPs in ${chrom}:${SS_start}-${SS_end}`, data["num_SS_snps"]],
-    ["First stage -log10(SS P-value) threshold", data["set_based_p"]],
-    ["Many SNPs not matching GTEx SNPs", data["snp_warning"]],
-    ["SNPs matching threshold level", data["thresh"]],
-    ["Number of SNPs matching with GTEx", data["numGTExMatches"]],
-    [
-      "Number of user-provided secondary datasets",
-      data["secondary_dataset_titles"].length,
-    ],
-    ["Run COLOC2", data["runcoloc2"]],
-  ];
+  let table_data = [];
+  if (type === "default") {
+    table_data = [
+      ["Session ID", sessionid],
+      ["Lead SNP", data["lead_snp"]],
+      ["Chromosome", chrom],
+      ["Start position", startbp],
+      ["End position", endbp],
+      ["Build", data["coordinate"]],
+      ["Infer variants", data["inferVariant"]],
+      [`Number of SNPs in ${chrom}:${startbp}-${endbp}`, data["snps"].length],
+      ["LD Population", data["ld_populations"]],
+      ["GTEx version", data["gtex_version"]],
+      ["Number of GTEx tissues selected", data["gtex_tissues"].length],
+      [
+        "Number of GTEx genes selected",
+        data["gtex_genes"] !== undefined
+          ? data["gtex_genes"].length
+          : undefined,
+      ],
+      ["SS region", data["SS_region"]],
+      [`Number of SNPs in ${chrom}:${SS_start}-${SS_end}`, data["num_SS_snps"]],
+      ["First stage -log10(SS P-value) threshold", data["set_based_p"]],
+      ["Many SNPs not matching GTEx SNPs", data["snp_warning"]],
+      ["SNPs matching threshold level", data["thresh"]],
+      ["Number of SNPs matching with GTEx", data["numGTExMatches"]],
+      [
+        "Number of user-provided secondary datasets",
+        data["secondary_dataset_titles"].length,
+      ],
+      ["Run COLOC2", data["runcoloc2"]],
+    ];
+  } else if (type === "set-based-test") {
+    table_data = []; // TODO
+  } else {
+    // Shouldn't get here
+    throw Error(`Unexpected params table type: ${type}`);
+  }
 
   for (const [key, value] of table_data) {
     if (value === undefined) continue;
