c38e423949fc404433f36c313eaae81aedadffa6
Mackenzie Ian Frew <mackenzie.frew@sickkids.ca>
2023-11-23 10:54:46 -0500

    Add async error check for multiple regions

--- static/js/inputChecker.js
+++ static/js/inputChecker.js
@@ -38,3 +38,27 @@ function checkNumSamplesInput(numsamples) {
         errordiv.text("Must be integer")
     }
 }
+
+/**
+ * Validate input format for multiple region inputs.
+ * No overlaps, no negative BP positions, and no unusual chromosome values.
+ */
+async function checkMultipleRegionsInput() {
+    let regiontext = $("#multi-region").val();
+    let errordiv = $("#multi-region-error");
+    errordiv.text("");
+    let build = d3.select("#coordinate").property("value");
+    let regions = regiontext.trim().split("\n");
+    let errors = [];
+
+    for (let i = 0; i < regions.length; i++) {
+        let region = regions[i];
+        let response = await d3.json(`/regionCheck/${build}/${region}`);
+        let message = response['response'];
+        if (message !== "OK") {
+            errors.push(`Line ${i+1}: ${message}`);
+        }    
+    }
+
+    errordiv.text(errors.join("\n<br />\n"));
+}
