d69ef83026753cef11712b769ba08e885aa6964b
Mackenzie Ian Frew <mackenzie.frew@sickkids.ca>
2024-03-13 10:38:11 -0400

    Update heatmap color scale with fixed cutoffs

--- static/js/plot_heatmap.js
+++ static/js/plot_heatmap.js
@@ -13,6 +13,10 @@ function copy(aObject) {
   return bObject;
 }
 
+function normalize(value, min, max) {
+  return Math.abs((value - min)/(max - min));
+}
+
 function plot_heatmap(
   genes,
   tissues,
@@ -25,8 +29,8 @@ function plot_heatmap(
   // want the different negative p-value statuses to have white/black/grey colors
   // can't really do it that way b/c of the different cases that may occur
   // prior checks ensure that we have at least one positive -log10 SS Pvalue
-  var pmax = 0;
-  var newSSPvalues = copy(SSPvalues);
+  let pmax = 0;
+  let newSSPvalues = copy(SSPvalues);
   for (i = 0; i < tissues.length; i++) {
     for (j = 0; j < genes.length; j++) {
       newSSPvalues[i][j] = parseFloat(newSSPvalues[i][j]);
@@ -39,19 +43,50 @@ function plot_heatmap(
       }
     }
   }
-  var colorscale_exception_percentage = 0.05;
-  var new_minp =
-    -1 *
-    (colorscale_exception_percentage / (1 - colorscale_exception_percentage)) *
-    pmax;
-  for (i = 0; i < tissues.length; i++) {
-    for (j = 0; j < genes.length; j++) {
-      if (newSSPvalues[i][j] === -1) {
-        newSSPvalues[i][j] = new_minp;
-      }
-    }
-  }
-  var data = [
+  // let colorscale_exception_percentage = 0.05;
+  // let new_minp =
+  //   -1 *
+  //   (colorscale_exception_percentage / (1 - colorscale_exception_percentage)) *
+  //   pmax;
+  // for (i = 0; i < tissues.length; i++) {
+  //   for (j = 0; j < genes.length; j++) {
+  //     if (newSSPvalues[i][j] === -1) {
+  //       newSSPvalues[i][j] = new_minp;
+  //     }
+  //   }
+  // }
+
+  // Plotly normalizes values before applying color scale. 
+  // we have to normalize in advance to know what colors should be present
+
+  let color_cutoffs = [
+    [-1, "rgb(105,105,105)"], // gray
+    [0.05, "rgb(105,105,105)"],
+
+    // Next trying to follow LD colors
+
+    // dark blue to bright red HSV gradient in 5 steps:
+    [0.05, "rgb(0, 0, 128)"], // dark blue (not significant)
+    [1.3, "rgb(0, 0, 128)"], // dark blue
+
+    [1.3, "rgb(0, 147, 142)"], // teal (p < 0.05)
+    [5, "rgb(0, 147, 142)"],
+
+    [5, "rgb(10, 166, 0)"], // green (p < 1e-5)
+    [6, "rgb(10, 166, 0)"],
+
+    [6, "rgb(185, 167, 0)"], // yellow/orange (p < 1e-6)
+    [7.3, "rgb(185, 167, 0)"],
+
+    [7.3, "rgb(204, 0, 24)"], // dark red (p < 5e-8)
+  ].filter(([threshold, _]) => (threshold >= -1 && threshold <= pmax)) // remove colors that are out of bounds of data
+  .map(([threshold, color]) => [normalize(threshold, -1, pmax), color]);
+
+  color_cutoffs.push([1, color_cutoffs.slice(-1)[0][1]]);
+  
+  console.debug(color_cutoffs);
+
+  let data = [
     {
       z: newSSPvalues,
       x: genes.map((gene) => `${gene}`),
@@ -67,66 +102,10 @@ function plot_heatmap(
         dtick: 1,
         autotick: false,
       },
-      colorscale: [
-        // Values between 0-15% of the min and max of z; ie. our negative stasuses
-
-        //   [0, 'rgb(0,0,0)'], // originally -3; black
-        //   [0.05, 'rgb(0,0,0)'], // first 5% is black
-
-        //   [0.05, 'rgb(255,255,255)'], // originally -2; white
-        //   [0.10, 'rgb(255,255,255)'], // between 5-10%
-
-        [0, "rgb(105,105,105)"], // originally -1; gray
-        [0.05, "rgb(105,105,105)"],
-
-        // Next trying to follow LD colors
-
-        // dark blue to bright red HSV gradient in 5 steps:
-        [0.05, "rgb(0, 0, 128)"], // dark blue
-        [0.24, "rgb(0, 0, 128)"], // dark blue
-
-        [0.24, "rgb(0, 147, 142)"],
-        [0.43, "rgb(0, 147, 142)"],
-
-        [0.43, "rgb(10, 166, 0)"],
-        [0.62, "rgb(10, 166, 0)"],
-
-        [0.62, "rgb(185, 167, 0)"],
-        [0.81, "rgb(185, 167, 0)"],
-
-        [0.81, "rgb(204, 0, 24)"],
-        [1.0, "rgb(204, 0, 24)"], // using darker red instead of rgb(255,0,0) bright red
-
-        // // Values between 20-30% of the min and max of z
-        // // have color rgb(40, 40, 40)
-
-        // [0.2, 'rgb(40, 40, 40)'],
-        // [0.3, 'rgb(40, 40, 40)'],
-
-        // [0.3, 'rgb(60, 60, 60)'],
-        // [0.4, 'rgb(60, 60, 60)'],
-
-        // [0.4, 'rgb(80, 80, 80)'],
-        // [0.5, 'rgb(80, 80, 80)'],
-
-        // [0.5, 'rgb(100, 100, 100)'],
-        // [0.6, 'rgb(100, 100, 100)'],
-
-        // [0.6, 'rgb(120, 120, 120)'],
-        // [0.7, 'rgb(120, 120, 120)'],
-
-        // [0.7, 'rgb(140, 140, 140)'],
-        // [0.8, 'rgb(140, 140, 140)'],
-
-        // [0.8, 'rgb(160, 160, 160)'],
-        // [0.9, 'rgb(160, 160, 160)'],
-
-        // [0.9, 'rgb(180, 180, 180)'],
-        // [1.0, 'rgb(180, 180, 180)']
-      ],
+      colorscale: color_cutoffs,
     },
   ];
-  var layout = {
+  let layout = {
     annotations: [],
     margin: {
       r: 50,
