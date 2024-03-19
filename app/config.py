503873057d924bc1650a94ae5b5e22172280b35d
Mackenzie Ian Frew <mackenzie.frew@sickkids.ca>
2024-03-19 09:40:05 -0400

    WIP app factory

---
+++ app/config.py
@@ -0,0 +1,76 @@
+"""
+Config classes for LocusFocus
+"""
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv(os.path.dirname(__file__))
+
+
+class BaseConfig:
+    """
+    Common configuration options for all environments.
+    """
+
+    # Content Security Policy to use with Flask-Talisman
+    CSP_POLICY = {
+        "default-src": "'self'",
+        "img-src": [
+            "*",
+            "data:",
+            "https://*.google-analytics.com",
+            "https://*.googletagmanager.com",
+        ],
+        "script-src": [
+            "'self'",
+            "'unsafe-inline'",  # TODO
+            "'unsafe-eval'",  # TODO
+            "https://*.googletagmanager.com",
+            "cdnjs.cloudflare.com",
+            "cdn.plot.ly",
+        ],
+        "style-src": [
+            "'self'",
+            "'unsafe-inline'",  # TODO
+            "use.fontawesome.com",
+            "cdnjs.cloudflare.com",
+            "stackpath.bootstrapcdn.com",
+        ],
+        "font-src": [
+            "'self'",
+            "use.fontawesome.com",
+            "cdnjs.cloudflare.com",
+            "stackpath.bootstrapcdn.com",
+        ],
+        "connect-src": [
+            "'self'",
+            "https://*.google-analytics.com",
+            "https://*.analytics.google.com",
+            "https://*.googletagmanager.com",
+        ],
+    }
+    UPLOAD_FOLDER = os.path.join(os.path.dirname(__file__), "static", "upload")
+    # Flask-Uploads
+    UPLOADED_FILES_DEST = UPLOAD_FOLDER
+    UPLOADED_FILES_ALLOW = set(['txt', 'tsv', 'ld', 'html'])
+    MAX_CONTENT_LENGTH = 500 * 1024 * 1024  # 500MB limit
+    
+    SEND_FILE_MAX_AGE_DEFAULT = 300  # 5 min cache
+    SECRET_KEY = os.environ.get("FLASK_SECRET_KEY", "")
+
+
+class DevConfig(BaseConfig):
+    """
+    Configuration options for development environment only.
+    """
+
+    pass
+
+
+class ProdConfig(BaseConfig):
+    """
+    Configuration options for production environment only.
+    """
+
+    pass
