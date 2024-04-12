9b7a335be2f0538d22ac03f132b40bf33331a7a1
Mackenzie Ian Frew <mackenzie.frew@sickkids.ca>
2024-04-12 10:25:06 -0400

    Squashed commit of the following:
    commit 28b21616e081374f99e2fb20c6098f9a72191585
    Merge: ea6de59 aff74da
    Author: Mackenzie Ian Frew <mackenzie.frew@sickkids.ca>
    Date:   Fri Apr 12 10:17:10 2024 -0400
    
        Merge branch 'mack/factory-pattern-refactor' of https://github.com/strug-hub/LocusFocus into mack/factory-pattern-refactor
    
    commit ea6de596de41ae93c6da2a22f15c19c9a6009468
    Author: Mackenzie Ian Frew <mackenzie.frew@sickkids.ca>
    Date:   Fri Apr 12 10:00:42 2024 -0400
    
        Tidy up imports, old pymongo
    
    commit 1b69bc3af18ef6af004d9265fe5c9c24d1aa0dac
    Author: Mackenzie Ian Frew <mackenzie.frew@sickkids.ca>
    Date:   Fri Apr 12 09:42:09 2024 -0400
    
        Use Flask-PyMongo for mongo operations
    
    commit bae60ddca3c9a585dbd4343a13dc4ecf79f9cf9d
    Author: Mackenzie Ian Frew <mackenzie.frew@sickkids.ca>
    Date:   Fri Apr 12 09:22:19 2024 -0400
    
        Move static, templates, coloc2, simplesum
    
    commit e05f51dd55ce7e29dc5dd11795ff3b9f57e43217
    Author: Mackenzie Ian Frew <mackenzie.frew@sickkids.ca>
    Date:   Thu Apr 4 13:57:44 2024 -0400
    
        Remove old app.py; finish moving to app directory
    
    commit d9be5845ce1e6ac5427b4da0834998f3d93785f0
    Author: Mackenzie Ian Frew <mackenzie.frew@sickkids.ca>
    Date:   Tue Mar 19 09:40:05 2024 -0400
    
        WIP app factory
    
    commit aff74da3e7c21129883a9801e62463e0260e3f38
    Author: Mackenzie Ian Frew <mackenzie.frew@sickkids.ca>
    Date:   Fri Apr 12 10:00:42 2024 -0400
    
        Tidy up imports, old pymongo
    
    commit d65df4660ef13bb58057835dd8eb357df11206e6
    Author: Mackenzie Ian Frew <mackenzie.frew@sickkids.ca>
    Date:   Fri Apr 12 09:42:09 2024 -0400
    
        Use Flask-PyMongo for mongo operations
    
    commit 6410e9ef22e5d15e7496d2692de74c6aa1284fa1
    Author: Mackenzie Ian Frew <mackenzie.frew@sickkids.ca>
    Date:   Fri Apr 12 09:22:19 2024 -0400
    
        Move static, templates, coloc2, simplesum
    
    commit 49ba36d83e1184dbe4cea5ae6a3d3e551b4338a1
    Author: Mackenzie Ian Frew <mackenzie.frew@sickkids.ca>
    Date:   Thu Apr 4 13:57:44 2024 -0400
    
        Remove old app.py; finish moving to app directory
    
    commit 503873057d924bc1650a94ae5b5e22172280b35d
    Author: Mackenzie Ian Frew <mackenzie.frew@sickkids.ca>
    Date:   Tue Mar 19 09:40:05 2024 -0400
    
        WIP app factory

---
+++ app/__init__.py
@@ -0,0 +1,37 @@
+"""
+Flask application factory.
+"""
+
+from flask import Flask
+from flask_pymongo import PyMongo
+from flask_sitemap import Sitemap
+from flask_talisman import Talisman
+from flask_uploads import UploadSet, DATA, configure_uploads
+from .config import ProdConfig
+
+
+ext = Sitemap()
+talisman = Talisman()
+files = UploadSet("files", DATA)
+mongo = PyMongo()
+
+
+def create_app(config_class=ProdConfig):
+    """
+    Create an instance of a Flask app for LocusFocus.
+    """
+    app = Flask(__name__, instance_relative_config=False)
+
+    app.config.from_object(config_class())
+    if app.config["SECRET_KEY"] is None or app.config["SECRET_KEY"] == "":
+        raise Exception("SECRET_KEY is not set! Add FLASK_SECRET_KEY to environment!")
+
+    ext.init_app(app)
+    talisman.init_app(app, content_security_policy=app.config["CSP_POLICY"])
+    configure_uploads(app, files)
+    mongo.init_app(app)
+
+    with app.app_context():
+        from . import routes
+
+        return app
