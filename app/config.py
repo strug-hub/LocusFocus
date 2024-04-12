bae60ddca3c9a585dbd4343a13dc4ecf79f9cf9d
Mackenzie Ian Frew <mackenzie.frew@sickkids.ca>
2024-04-12 09:22:19 -0400

    Move static, templates, coloc2, simplesum

--- app/config.py
+++ app/config.py
@@ -55,9 +55,14 @@ class BaseConfig:
     UPLOADED_FILES_DEST = UPLOAD_FOLDER
     UPLOADED_FILES_ALLOW = set(['txt', 'tsv', 'ld', 'html'])
     MAX_CONTENT_LENGTH = 500 * 1024 * 1024  # 500MB limit
-    
+
     SEND_FILE_MAX_AGE_DEFAULT = 300  # 5 min cache
     SECRET_KEY = os.environ.get("FLASK_SECRET_KEY", "")
+
+    LF_DATA_FOLDER = os.path.join(
+            os.path.dirname(os.path.dirname(__file__)),  # project root below app folder
+            "data"
+        )
 
 
 class DevConfig(BaseConfig):
