7f227a708415a6aa113e1780b9ee39f2d18a57db
Mackenzie Ian Frew <mackenzie.frew@sickkids.ca>
2024-06-25 11:59:24 -0400

    Add session folder to config - Use strict index on environ for secret_key; hoping for error if it's not defined

--- app/config.py
+++ app/config.py
@@ -51,19 +51,20 @@ class BaseConfig:
         ],
     }
     UPLOAD_FOLDER = os.path.join(os.path.dirname(__file__), "static", "upload")
+    SESSION_FOLDER = os.path.join(os.path.dirname(__file__), "static", "session_data")
     # Flask-Uploads
     UPLOADED_FILES_DEST = UPLOAD_FOLDER
     UPLOADED_FILES_ALLOW = set(['txt', 'tsv', 'ld', 'html'])
     MAX_CONTENT_LENGTH = 500 * 1024 * 1024  # 500MB limit
 
     SEND_FILE_MAX_AGE_DEFAULT = 300  # 5 min cache
-    SECRET_KEY = os.environ.get("FLASK_SECRET_KEY", "")
+    SECRET_KEY = os.environ["FLASK_SECRET_KEY"]
 
     LF_DATA_FOLDER = os.path.join(
             os.path.dirname(os.path.dirname(__file__)),  # project root below app folder
             "data"
         )
-
+    
 
 class DevConfig(BaseConfig):
     """
