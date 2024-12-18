b909fb00a425d3683fdf2ca6d24d6a2617f8ac7a
Mackenzie Ian Frew <mackenzie.frew@sickkids.ca>
2024-12-18 11:13:46 -0500

    Running pipeline WIP

--- app/tasks.py
+++ app/tasks.py
@@ -2,28 +2,29 @@
 Celery tasks for LocusFocus.
 """
 
-from uuid import UUID
+from typing import Iterable
 from celery import shared_task
+from celery.result import AsyncResult
 
 from app.pipeline import Pipeline
 
-def run_pipeline_async(pipeline: Pipeline, initial_payload: object) -> UUID:
+def run_pipeline_async(pipeline: Pipeline, payload_params: Iterable) -> AsyncResult:
     """
     Run a pipeline asynchronously using Celery.
     Return the task ID, which can be used to check the task status.
     """
-    result = _pipeline_task.apply_async(pipeline, initial_payload, task_id=pipeline.id)  # type: ignore
-    return result.id
+    result = _pipeline_task.apply_async(pipeline, payload_params, task_id=pipeline.id)  # type: ignore
+    return result
 
 
 @shared_task(ignore_result=False)
-def _pipeline_task(pipeline: Pipeline, initial_payload: object) -> object:
+def _pipeline_task(pipeline: Pipeline, payload_params: Iterable) -> object:
     """
     Celery task for running a pipeline.
     
     Do not call this function directly. Instead, use `run_pipeline_async`.
     """
 
-    result = pipeline.process(initial_payload)
+    result = pipeline.process(*payload_params)
 
     return result
