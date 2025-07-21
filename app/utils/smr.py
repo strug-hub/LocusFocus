5ece539cf1c8c6792584c8628152a60c69e606b4
cklamann <12862284+cklamann@users.noreply.github.com>
2025-07-21 11:26:43 -0400

    add test for smr query

--- app/utils/smr.py
+++ app/utils/smr.py
@@ -56,20 +56,65 @@ datasets: dict[str, SMRDataset] = {
 }
 
 
-def query_smr(chr: int, snps: List[str], dataset: str, thresh: float = 5.0e-8):
+def run_smr_query(
+    query_path: str, chr: int, thresh: float, start: int, end: int
+) -> pd.DataFrame:
+    """Query the data, save to a temporary file, and return as a dataframe
+
+    :param query_path: The path to the data to be queried
+    :type query_path: str
+    :param chr: The chromosome to query
+    :type chr: int
+    :param thresh: The p-value threshold
+    :type thresh: float
+    :param start: The start bp to query
+    :type start: int
+    :param end: Then end bp to query
+    :type end: int
+    :return: The returned SNPs and pvalues
+    :rtype: pd.DataFrame
+    """
+    with NamedTemporaryFile("w") as f:
+        query = [
+            "smr",
+            "--beqtl-summary",
+            query_path,
+            "--query",
+            str(thresh),
+            "--snp-chr",
+            str(chr),
+            "--from-snp-kb",
+            str(start),
+            "--to-snp-kb",
+            str(end),
+            "--out",
+            f.name,
+        ]
+
+        subprocess.run(query, check=True)
+
+        return pd.read_csv(f"{f.name}.txt", sep="\t")
+
+
+def query_smr(
+    chr: int, snps: List[str], dataset: str, thresh: float = 5.0e-8
+) -> pd.DataFrame:
     """Query mqtl data in smr format
 
     :param chr: The chromosome to query
     :type chr: int
     :param snps: A list of SNPS in format chr{chr}_{bp}_ref_alt
     :type snps: List[str]
-    :param snps_rs: A list of the *same* SNPs in rsid format
-    :type snps_rs: List[str]
     :param dataset: The dataset to query
     :type dataset: str
     :param thresh: The p-value threshold, defaults to 5e-8
     :type thresh: float
     :raises FileNotFoundError: If the dataset does not exist
+    :return: The SNPs and pvalues as a dataframe with the following columns:
+    'SNP', 'Chr', 'BP', 'A1', 'A2', 'Freq', 'Probe', 'Probe_Chr',
+    'Probe_bp', 'Gene', 'Orientation', 'b', 'SE', 'p', 'full_snp'
+    Note that 'full_snp' is a combined column that takes the same format as those in ``snps``
+    :rtype: pd.DataFrame
     """
     if dataset not in datasets.keys():
         raise FileNotFoundError(f"Dataset {dataset} does not exist!")
@@ -85,39 +130,21 @@ def query_smr(chr: int, snps: List[str], dataset: str, thresh: float = 5.0e-8):
     start = min(snp_poses) // 1000
     end = max(snp_poses) // 1000 + 1
 
-    with NamedTemporaryFile("w") as f:
-        query = [
-            "smr",
-            "--beqtl-summary",
-            base_filepath,
-            "--query",
-            str(thresh),
-            "--snp-chr",
-            str(chr),
-            "--from-snp-kb",
-            str(start),
-            "--to-snp-kb",
-            str(end),
-            "--out",
-            f.name,
-        ]
+    query_result = run_smr_query(
+        query_path=base_filepath, chr=chr, thresh=thresh, start=start, end=end
+    )
 
-        subprocess.run(query, check=True)
-
-        res = pd.read_csv(f"{f.name}.txt", sep="\t")
-
-    res.sort_values(by="BP", inplace=True)
-    res["full_snp"] = res.apply(
-        lambda x: f"chr{str(x['Chr'])}"
+    query_result["full_snp"] = query_result.apply(
+        lambda df: f"chr{str(df['Chr'])}"
         + "_"
-        + str(x["BP"])
+        + str(df["BP"])
         + "_"
-        + x["A1"]
+        + df["A1"]
         + "_"
-        + x["A2"],
+        + df["A2"],
         axis=1,
     )
 
-    filtered = res[res["full_snp"].isin(snps)]
+    filtered = query_result[query_result["full_snp"].isin(snps)]
 
     return filtered
