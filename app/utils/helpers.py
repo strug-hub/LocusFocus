649303ccad2f2116f43c6594f60a2b48516a5ef8
Mackenzie Ian Frew <mackenzie.frew@sickkids.ca>
2025-09-17 16:37:27 -0400

    Add case for handling missing ref, alt columns

--- app/utils/helpers.py
+++ app/utils/helpers.py
@@ -1,4 +1,6 @@
-from typing import Any, List
+from typing import List
+
+import pandas as pd
 
 
 def validate_chromosome(
@@ -30,3 +32,60 @@ def validate_chromosome(
         raise ValueError(f"Chromosome must be one of {', '.join(chrs)}")
 
     return True
+
+
+def adjust_snp_column(
+        snps_df: pd.DataFrame,
+        target_build: str,
+        snp_col: str = "SNP",
+        chrom_col: str = "CHROM",
+        pos_col: str = "POS",
+        ref_col: str = "REF",
+        alt_col: str = "ALT",
+        ignore_alleles: bool = False,
+    ) -> pd.DataFrame:
+    """
+    Adjust the SNP column to be consistent with the other columns in the dataframe.
+    rsIDs are ignored. Necessary to perform after LiftOver.
+
+    :param snps_df: The dataframe containing the SNP column
+    :type snps_df: pd.DataFrame
+    :param target_build: The target build, either "hg19" or "hg38"
+    :type target_build: str
+    :param snp_col: The name of the SNP column, defaults to "SNP"
+    :type snp_col: str, optional
+    :param chrom_col: The name of the chromosome column, defaults to "CHROM"
+    :type chrom_col: str, optional
+    :param pos_col: The name of the position column, defaults to "POS"
+    :type pos_col: str, optional
+    :param ref_col: The name of the reference allele column, defaults to "REF"
+    :type ref_col: str, optional
+    :param alt_col: The name of the alternate allele column, defaults to "ALT"
+    :type alt_col: str, optional
+    :param ignore_alleles: Whether alleles should be ignored. 
+        If true, alleles will be pulled from the SNP column instead of the ref/alt columns. 
+        Defaults to False
+    :type ignore_alleles: bool, optional
+    :return: The dataframe with the SNP column adjusted.
+    :rtype: pd.DataFrame
+    """
+    if target_build.lower() not in ["hg19", "hg38"]:
+        raise ValueError("Target build must be either 'hg19' or 'hg38'")
+
+    build_suffix = "b37" if target_build.lower() == "hg19" else "b38"
+
+    # Mask out rows with rsIDs in the SNP column
+    rsid_mask = snps_df[snp_col].str.contains("rs")
+    if rsid_mask.all():
+        # nothing to do, rsIDs are semi-stable 
+        return snps_df
+
+    if not ignore_alleles:
+        snps_df.loc[~rsid_mask, snp_col] = snps_df.loc[~rsid_mask, chrom_col].astype(str) + "_" + snps_df.loc[~rsid_mask, pos_col].astype(str) + "_" + snps_df.loc[~rsid_mask, ref_col].astype(str) + "_" + snps_df.loc[~rsid_mask, alt_col].astype(str) + "_" + build_suffix
+    else:
+        # get alleles from snp column
+        alleles = snps_df[snp_col].str.split("_", expand=True)
+        alleles.columns = ["chrom", "pos", "ref", "alt", "build"]
+        snps_df.loc[~rsid_mask, snp_col] = snps_df.loc[~rsid_mask, chrom_col].astype(str) + "_" + snps_df.loc[~rsid_mask, pos_col].astype(str) + "_" + alleles.loc[~rsid_mask, "ref"].astype(str) + "_" + alleles.loc[~rsid_mask, "alt"].astype(str) + "_" + build_suffix
+
+    return snps_df
