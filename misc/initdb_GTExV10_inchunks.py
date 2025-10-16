dfd8c19574c498a0f1ba9333f13b31bb24122765
Mackenzie Ian Frew <mackenzie.frew@sickkids.ca>
2025-10-16 09:51:35 -0400

    update mongo script

--- misc/initdb_GTExV10_inchunks.py
+++ misc/initdb_GTExV10_inchunks.py
@@ -8,18 +8,20 @@ Adapted from: https://maxhalford.github.io/blog/pandas-streaming-groupby/
 Works well in Ubuntu
 """
 
+import cProfile
 import itertools
 # import multiprocessing as mp # Mongo does not support several write requests
 import pandas as pd
 import numpy as np
 import os
+from pathlib import Path
 import pymongo
 from pymongo import MongoClient
 import subprocess
 from datetime import datetime
 
 
-def stream_groupby_csv(path, key, agg, chunk_size=1e6, pool=None, **kwargs):
+def stream_groupby_parquet(path, key, agg, chunk_size=1e6, pool=None, **kwargs):
 
     # Make sure path is a list
     if not isinstance(path, list):
@@ -28,7 +30,7 @@ def stream_groupby_csv(path, key, agg, chunk_size=1e6, pool=None, **kwargs):
     # Chain the chunks
     kwargs['chunksize'] = chunk_size
     chunks = itertools.chain(*[
-        pd.read_csv(p, **kwargs)
+        pd.read_parquet(p, **kwargs)
         for p in path
     ])
 
@@ -68,28 +70,31 @@ def agg(chunk):
     return chunk.groupby('gene_id').apply(push_variant_dict)
 
 
-def push_variant_dict(gene_df):
-    variant_id = [str(x).replace('chr','').encode('utf-8') for x in list(gene_df['variant_id'])]
-    pval = list(gene_df['pval_nominal'])
-    beta = list(gene_df['slope'])
-    se = list(gene_df['slope_se'])
-    ma_samples = list(gene_df['ma_samples'])
-    ma_count = list(gene_df['ma_count'])
-    sample_maf = list(gene_df['maf'])
-    geneid = gene_df.reset_index()['gene_id'][0]
-    variants_list = []
-    for row in np.arange(len(variant_id)):
-        variants_list.append({
-            'variant_id': variant_id[row].decode('utf-8')
-            , 'pval': float(pval[row])
-            , 'beta': float(beta[row])
-            , 'se': float(se[row])
-            , 'ma_samples': float(ma_samples[row])
-            , 'ma_count': float(ma_count[row])
-            , 'sample_maf': float(sample_maf[row])                     
-            })
-    gene_dict = {'gene_id': geneid, 'eqtl_variants': variants_list }
-    collection.insert_one(gene_dict)
+def push_variant_dict(collection):
+    def wrapped(gene_df):
+        
+        variant_id = [str(x).replace('chr','').encode('utf-8') for x in list(gene_df['variant_id'])]
+        pval = list(gene_df['pval_nominal'])
+        beta = list(gene_df['slope'])
+        se = list(gene_df['slope_se'])
+        ma_samples = list(gene_df['ma_samples'])
+        ma_count = list(gene_df['ma_count'])
+        sample_maf = list(gene_df['af'])
+        geneid = gene_df.reset_index()['gene_id'][0]
+        variants_list = []
+        for row in np.arange(len(variant_id)):
+            variants_list.append({
+                'variant_id': variant_id[row].decode('utf-8')
+                , 'pval': float(pval[row])
+                , 'beta': float(beta[row])
+                , 'se': float(se[row])
+                , 'ma_samples': float(ma_samples[row])
+                , 'ma_count': float(ma_count[row])
+                , 'sample_maf': float(sample_maf[row])                     
+                })
+        gene_dict = {'gene_id': geneid, 'eqtl_variants': variants_list }
+        collection.insert_one(gene_dict)
+    return wrapped
 
 
 ##########################################
@@ -100,81 +105,69 @@ conn = "mongodb://localhost:27017"
 client = MongoClient(conn)
 db = client.GTEx_V10
 
-tissues = pd.read_csv(os.path.join('data', 'GTEx_v10_eQTL','tissues4.txt'), header=None)
-tissues = list(tissues.iloc[:,0])
-files_list = [ #'GTEx_Analysis_v10_eQTL_all_associations_' +
-              tissue.replace(' ','_') + '.allpairs_fixed.txt.gz' for tissue in tissues ]
-# files_list = [ 'Pancreas.allpairs_fixed.txt.gz', 'Lung.allpairs_fixed.txt.gz' ]
+# Adipose_Subcutaneous.v10.allpairs.chr1.parquet
+# <tissue>.v10.allpairs.<chrom>.parquet
 
+GTEX_ROOT = os.path.join('data', 'GTEx_v10_eQTL')
+files_list = list(map(lambda name: os.path.join(GTEX_ROOT, name), filter(lambda x: x.endswith('.parquet'), os.listdir(GTEX_ROOT))))
+tissues = list(map(lambda x: x.split('.')[0].replace(' ','_'), files_list))
+completed_files_file = os.path.join(GTEX_ROOT, 'completed_files.txt')
+if not os.path.isfile(completed_files_file):
+    # touch file
+    Path(completed_files_file).touch()
 
-for file in files_list:
-    tissue_name = file.split('.')[0].replace(' ','_').replace('GTEx_Analysis_v10_eQTL_all_associations_','')
-    file = os.path.join('data','GTEx_v10_eQTL', file)
-    if tissue_name not in db.list_collection_names():
-        collection = db[tissue_name]
-        if file.endswith('gz') and os.path.isfile(file):
-            print('Decompressing ' + file)
-            subprocess.run(args=['gunzip','-f',file])
-            #decompress(file)
-        print('Parsing file ' + file + ' and creating tissue collection')
-        results = stream_groupby_csv(
-            path=[ file.replace('.gz','') ],
-            key='gene_id',
-            agg=agg,
-            chunk_size=1e6,
-            sep="\t",
-            usecols=['gene_id', 'variant_id', 'pval_nominal',
-                     'slope', 'slope_se', 'ma_samples', 'ma_count', 'af']
+completed_filenames = set()
+with open(completed_files_file, 'r', encoding='utf-8') as f:
+    for line in f:
+        completed_filenames.add(line.strip())
+
+files_df = pd.DataFrame(
+    {
+        "filename": files_list,
+        "tissue": [x.split("/")[-1].split('.')[0].replace(' ','_') for x in files_list],
+        "chrom": [x.split('.')[3] for x in files_list]
+    }
+)
+
+test_tile = None
+test_file = files_df.iloc[202:203]
+
+def write_to_db(df):
+    """filename df groupby helper"""
+    tissue = df.iloc[0]['tissue']
+    if tissue not in db.list_collection_names():
+        collection = db.create_collection(tissue)
+        print(tissue + ' collection created')
+    collection = db[tissue]
+    chroms = df["chrom"].unique()
+    for chrom in chroms:
+        file = df[df['chrom'] == chrom]['filename'].iloc[0]
+        print('Parsing file ' + file)
+        if file in completed_filenames:
+            print('Skipping file ' + file)
+            continue
+        
+        tissue_eqtls = pd.read_parquet(
+            file,
+            columns=['gene_id', 'variant_id', 'af', 'ma_samples', 'ma_count', 'pval_nominal', 'slope', 'slope_se']
         )
-        print(tissue_name + ' collection created')
-        print(datetime.now().strftime('%c'))
-        print('Now indexing by gene_id')
-        print(datetime.now().strftime('%c'))
-        collection.create_index('gene_id')
-        print('Indexing done')
-        print(datetime.now().strftime('%c'))
-        print('Recompressing ' + file.replace('.gz',''))
-        subprocess.run(args=['gzip', file.replace('.gz','')])
-        #print('Deleting ' + file.replace('.gz',''))
-        #subprocess.run(args=['rm', '-f', file.replace('.gz','')])
-        print('Done with tissue ' + tissue_name)
-        print(datetime.now().strftime('%c'))
-    #break
+        tissue_eqtls.groupby('gene_id').apply(push_variant_dict(collection))  # type: ignore
+        with open("completed_files.txt", "a", encoding='utf-8') as f:
+            f.write(file + '\n')
 
-# Next, create the variant lookup table
-# print('Reading variant lookup file GTEx_Analysis_2017-06-05_v8_WholeGenomeSeq_838Indiv_Analysis_Freeze.lookup_table.txt.gz')
-# collection = db['variant_table']
-# tbl_chunk = pd.read_csv(os.path.join('data','GTEx_v10_eQTL','GTEx_Analysis_2017-06-05_v8_WholeGenomeSeq_838Indiv_Analysis_Freeze.lookup_table.txt.gz'), 
-#                         sep="\t", chunksize=1e5, encoding='utf-8')
-# print('Pushing variant information into variant_table collection by chunks')
-# for tbl in tbl_chunk:
-#     tbl['chr'] = [int(str(x).replace('chr','').replace('X','23')) for x in list(tbl['chr'])]
-#     tbl['variant_id'] = [x.replace('chr','') for x in list(tbl['variant_id'])]
-#     tbl_dict = tbl.to_dict(orient='records')
-#     collection.insert_many(tbl_dict)
-# print('Variant collection created')
-print(datetime.now().strftime('%c'))
-
-print('Now indexing by variant_id')
-collection.create_index('variant_id')
-print('Indexing by variant_id done')
-print(datetime.now().strftime('%c'))
-print('Now indexing by chr and pos (ascending) order')
-collection.create_index([("chr", pymongo.ASCENDING),
-                         ("variant_pos", pymongo.ASCENDING)])
-print('Indexing by chr and pos done')
-print(datetime.now().strftime('%c'))
+        del tissue_eqtls  # try and free up memory
+    print(datetime.now().strftime('%c'))
+    print('Now indexing by gene_id')
+    print(datetime.now().strftime('%c'))
+    collection.create_index('gene_id')
+    print('Indexing done')
+    print(datetime.now().strftime('%c'))
+    print('Done with tissue ' + tissue)
+    print(datetime.now().strftime('%c'))
 
 
+#test_file.apply(write_to_db)
+cProfile.run("test_file.groupby('tissue').apply(write_to_db)", filename="test.prof")
+#files_df.groupby("tissue").apply(write_to_db) # type: ignore
 
-# Test database results ok:
-# tissue_name = 'test'
-# collection = db[tissue_name]
-# collection.estimated_document_count()
-# gene = 'ENSG00000227232.5'
-# results = collection.find({'gene_id': gene})
-# temp = list(results)
-# len(temp)
-# temp[0]['gene_id'] == gene
-# temp[0]['eqtl_variants']
-# len(temp[0]['eqtl_variants'])
+# no variant lookup table
